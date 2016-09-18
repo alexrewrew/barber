fc6a1f42831faf65fe304efcc1274e98ce40e1d8
Vitalii Mykhailov <norvelx@gmail.com>
2016-09-18 21:48:39 +0300

    fucking big update

--- frontend/js/plugins.js
+++ frontend/js/plugins.js
@@ -25,18 +25,42 @@ $(document).ready(function () {
             {
                 src: "frontend/img/jumbo-slider/jumbo1.png"
             }
-            , {
-                src: "frontend/img/jumbo-slider/jumbo2.jpg"
-            }
-            , {
-                src: "frontend/img/jumbo-slider/jumbo3.jpg"
-            }
-            , {
-                src: "frontend/img/jumbo-slider/jumbo4.jpg"
-            }
-            , {
-                src: "frontend/img/jumbo-slider/jumbo5.jpg"
-            }
+            // , {
+            //     src: "frontend/img/jumbo-slider/jumbo2.jpg"
+            // }
+            // , {
+            //     src: "frontend/img/jumbo-slider/jumbo3.jpg"
+            // }
+            // , {
+            //     src: "frontend/img/jumbo-slider/jumbo4.jpg"
+            // }
+            // , {
+            //     src: "frontend/img/jumbo-slider/jumbo5.jpg"
+            // }
     ]
     });
 });
+
+
+$(document).ready(function () {
+
+    $(function () {
+        $('a.page-scroll').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1000, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    /***************** Wow.js ******************/
+
+    new WOW().init();
+});
+
