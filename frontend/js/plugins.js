9a8c7f062f1915c19a3b0ae106728d1b5df98c74
alexrewrew <alexrewrew@live.com>
2016-09-20 21:08:22 +0300

    changes

--- frontend/js/plugins.js
+++ frontend/js/plugins.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
 });
 $(document).ready(function () {
     $("#jumbo").vegas({
-        autoplay:true,
-        slides: [
+        autoplay: true
+        , slides: [
             {
                 src: "frontend/img/jumbo-slider/main.jpg"
             }
@@ -25,10 +25,7 @@ $(document).ready(function () {
     ]
     });
 });
-
-
 $(document).ready(function () {
-
     // $(function () {
     //     $('a.page-scroll').bind('click', function (event) {
     //         var $anchor = $(this);
@@ -38,53 +35,56 @@ $(document).ready(function () {
     //         event.preventDefault();
     //     });
     // });
-
-
-    $('.parallax-window').parallax({imageSrc: 'frontend/img/testimonials/basic.jpg'});
-    
-
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 100
+    $('.parallax-window').parallax({
+        imageSrc: 'frontend/img/testimonials/basic.jpg'
     });
-
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+        , offset: 100
+    });
     /***************** Wow.js ******************/
-
     new WOW().init();
+    
+    var $head = $("iframe").contents().find("head");
+    $head.append($("<link/>", {
+        rel: "stylesheet"
+        , href: "frontend/css/style.css"
+        , type: "text/css"
+    }));
 });
 //
 $(document).ready(function () {
-     $(document).on("scroll", onScroll);
-     $('.page-scroll').on('click', function (e) {
-         e.preventDefault();
-         $(document).off("scroll");
-         $('.page-scroll').each(function () {
-             $(this).removeClass('active');
-         })
-         $(this).addClass('active');
-         var target = this.hash;
-         $target = $(target);
-         $('html, body').stop().animate({
-             'scrollTop': $target.offset().top - 80
-         }, 500, 'swing', function () {
-             window.location.hash = target;
-             $(document).on("scroll", onScroll);
-         });
-     });
- });
+    $(document).on("scroll", onScroll);
+    $('.page-scroll').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        $('.page-scroll').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 80
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
 
- function onScroll(event) {
-     var scrollPosition = $(document).scrollTop();
-     $('.scrolll a').each(function () {
-         var currentLink = $(this);
-         var refElement = $(currentLink.attr("href"));
-         if (refElement.position().top - 40 <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-             $('.scrolll a').removeClass("active");
-             currentLink.addClass("active");
-         }
-         else {
-             currentLink.removeClass("active");
-         }
-     });
- }
+function onScroll(event) {
+    var scrollPosition = $(document).scrollTop();
+    $('.scrolll a').each(function () {
+        var currentLink = $(this);
+        var refElement = $(currentLink.attr("href"));
+        if (refElement.position().top - 40 <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+            $('.scrolll a').removeClass("active");
+            currentLink.addClass("active");
+        }
+        else {
+            currentLink.removeClass("active");
+        }
+    });
+}
 //
