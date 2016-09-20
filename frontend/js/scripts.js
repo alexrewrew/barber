18b05ee9faca767b32d84c169e9f46c64d32157b
alexrewrew <alexrewrew@live.com>
2016-09-20 22:03:18 +0300

    d

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,34 +1,30 @@
 $(document).ready(function () {
-
-    $('.barber-select').click(function(){
+    $('.barber-select').click(function () {
         var type = $(this).data('barber');
         var img = $(this).data('img');
-
         $('.barber-select').removeClass('text-green');
         $(this).addClass('text-green');
-
         console.log(type);
-
-        if ( type == 'basic')
-        {
+        if (type == 'basic') {
             $('#barbos_premium').hide();
             $('#barbor-premium').hide();
-
             $('#barbos_basic').show();
             $('#barbor-basic').show();
-        }else{
+        }
+        else {
             $('#barbos_basic').hide();
             $('#barbor-basic').hide();
-
             $('#barbos_premium').show();
             $('#barbor-premium').show();
         }
-
         $('.parallax-slider').attr('src', img);
-
-
         $('.language-list').toggleClass('visible');
     });
-
+    var ww = document.body.clientWidth;
+    if (ww < 768) {
+        $('.project-name').css('display', 'none');
+        $('.portfolio-box').click(function () {
+            $('.project-name').css('display', 'block');
+        })
+    }
 });
-
